ee3a2beeeeeb19538842cb0d2cb36b3c1dcc1aa4
Lawrence Macharia <macharialawrence2011@gmail.com>
2017-03-13 23:24:55 +0300

    Added the extra fields in the fillable array and formatted the array to face down

--- src/models/Payment.php
+++ src/models/Payment.php
@@ -9,7 +9,19 @@ class Payment extends Model
 {
     protected $table = 'payments';
 
-    protected $fillable = ['phone_no', 'client_name', 'transaction_id', 'amount', 'status', 'acc_no', 'transaction_type', 'transaction_time', 'is_matched'];
+    protected $fillable = [
+        'phone_no',
+        'sender_first_name',
+        'sender_middle_name',
+        'sender_last_name',
+        'transaction_id',
+        'amount',
+        'status',
+        'acc_no',
+        'transaction_type',
+        'transaction_time',
+        'is_matched'
+    ];
 
 
     /**
