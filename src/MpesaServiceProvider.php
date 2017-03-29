0ecf384b286b7e80e2e46eff770a5570c41ee150
Lawrence Macharia <macharialawrence2011@gmail.com>
2017-03-29 10:16:14 +0300

    Setting up the configurations

--- src/MpesaServiceProvider.php
+++ src/MpesaServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Mobidev\Mpesa;
 
 use Illuminate\Support\ServiceProvider;
+use Mobidev\Mpesa\Services\OnlineCheckout;
 
 class MpesaServiceProvider extends ServiceProvider
 {
@@ -15,14 +16,22 @@ class MpesaServiceProvider extends ServiceProvider
     {
 
         include __DIR__ . '/routes.php';
+
+        // publish migration files
         $this->publishes(
             [
                 __DIR__ . '/migrations' => $this->app->databasePath() . '/migrations'
             ], 'migrations');
 
+        // publish views
         $this->publishes([
             __DIR__ . '/views' => base_path('resources/views/mobidev/mpesa'),
         ], 'views');
+
+        // publish config files
+        $this->publishes([
+            __DIR__ . '/../../config/config.php' => config_path('mpesa.php'),
+        ]);
 
     }
 
@@ -33,6 +42,33 @@ class MpesaServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->make('Mobidev\Mpesa\controllers\C2BController');
+
+        $this->registerPackage();
+
+//        $this->registerConfig();
     }
+
+    /**
+     * Register package components
+     */
+    private function registerPackage()
+    {
+        // C2B Controller
+        $this->app->make('Mobidev\Mpesa\controllers\C2BController');
+
+        // Online Checkout Service
+        $this->app->singleton('online_checkout', function () {
+            return new OnlineCheckout();
+        });
+    }
+
+//    /**
+//     * Merge user custom and mpesa configs
+//     */
+//    private function registerConfig()
+//    {
+//        $this->mergeConfigFrom(
+//            __DIR__ . '/config/config.php', 'mpesa'
+//        );
+//    }
 }
