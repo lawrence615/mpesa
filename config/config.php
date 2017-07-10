ecad2eefc358c17a2c0645a953e5cc9c2e13febb
Lawrence Macharia <macharialawrence2011@gmail.com>
2017-07-10 03:21:19 +0300

    code indentation

--- config/config.php
+++ config/config.php
@@ -7,7 +7,7 @@ return [
     'online_checkout' => [
         'passkey' => env('MPESA_ONLINE_CHECKOUT_PASSKEY', ''), // provided by Safaricom
         'merchant_id' => env('MPESA_ONLINE_CHECKOUT_MERCHANT_ID', ''), // provided by Safaricom
-        'callback_url' => env('MPESA_ONLINE_CHECKOUT_CALLBACK_URL', '') // Configure an endpoint to receive the notification
+        'callback_url' => env('MPESA_ONLINE_CHECKOUT_CALLBACK_URL', '') // configure an endpoint to receive the notifications, POST route
     ],
     'c2b' => [
         'paybill' => env('MPESA_C2B_PAYBILL_NUMBER', '') // provided by Safaricom
