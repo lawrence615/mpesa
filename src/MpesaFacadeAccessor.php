76d42b88c03cc49cbc0b2708ee11b35d5a868da8
Lawrence Macharia <macharialawrence2011@gmail.com>
2017-07-10 03:21:57 +0300

    WIP

--- src/MpesaFacadeAccessor.php
+++ src/MpesaFacadeAccessor.php
@@ -4,6 +4,7 @@ namespace Mobidev\Mpesa;
 
 
 use App\Http\Requests;
+use Mobidev\Mpesa\Services\OnlineCheckout;
 
 /**
  * PROCESS FLOW
@@ -29,92 +30,18 @@ class MpesaFacadeAccessor
 
 
     /**
-     * Consists of passkey concatenated with MERCHANT_ID and TIMESTAMP
+     * Mpesa Online Checkout API provider object.
      *
-     * @var string
-     */
-    protected $password;
-    /**
-     * @var int
-     */
-    protected $amount;
-
-    /**
-     * @var int
-     */
-    protected $number;
-
-    /**
-     * @var int
-     */
-    protected $referenceId;
-
-    /**
-     * @var string
-     */
-    protected $passkey;
-
-    /**
      * @var
      */
-    protected $c2b_pay_bill;
+    public static $provider;
 
     /**
-     * @var string
+     * @return mixed
      */
-    protected $timestamp;
-
-    /**
-     * @var array
-     */
-    protected $requestKeys;
-
-    /**
-     * @var string
-     */
-    protected $request;
-
-
-    /**
-     * @var Client
-     */
-    protected $client;
-
-    /**
-     * @var string
-     */
-    protected $endpoint;
-
-
-    public function __construct($config_handler = null)
+    public static function getProvider()
     {
-        // check if C2B Paybill Number is set
-        if ($config_handler['c2b_pay_bill_number'] == null) {
-            throw new \Exception('C2B Paybill Number should be provided');
-        }
-
-        // check if the passkey is set also
-        if ($config_handler['passkey'] == null) {
-            throw new \Exception('Passkey should be provided');
-        }
-
-        // assign values needed for processing
-        $this->endpoint = "https://safaricom.co.ke/mpesa_online/lnmo_checkout_server.php?wsdl";
-        $this->c2b_pay_bill = $config_handler['c2b_pay_bill_number'];
-        $this->passkey = $config_handler['passkey'];
+        return new OnlineCheckout();
     }
 
-
-    public function process($amount, $number, $referenceId)
-    {
-        // @TODO validate the data captured
-
-        $this->amount = $amount;
-        $this->number = sprintf('254%s', substr(trim($number), -9));
-        $this->referenceId = $referenceId;
-        $this->initialize();
-
-        // execute the request
-        $this->execute();
-    }
 }
